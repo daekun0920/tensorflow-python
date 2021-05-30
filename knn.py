4272440194460e41310d7a5f6359bcf5738b9028
daekunhan <dosemfb0920@gmail.com>
2021-05-30 17:18:02 +0900

    init

---
+++ knn.py
@@ -0,0 +1,47 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+"""
+K-Nearest Neighbors Algorithm. 
+This is a classification algorithm that attempts to classify data points based upon its closest "neighbors"
+"""
+
+data = pd.read_csv("car.data")
+print(data.head())
+
+"""
+String 데이터 숫자로 전처리
+"""
+le = preprocessing.LabelEncoder()
+
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+predict = "class"
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+Y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=9)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N : ", n)
